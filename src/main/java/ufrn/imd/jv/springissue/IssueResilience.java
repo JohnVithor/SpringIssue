cbe57d926efad39465ba7c88d6f5eca3f84041b3
JohnVithor <jv.venceslau.c@gmail.com>
2022-12-08 10:36:54 -0300

    atualiza tipo de bulkhead

--- src/main/java/ufrn/imd/jv/springissue/IssueResilience.java
+++ src/main/java/ufrn/imd/jv/springissue/IssueResilience.java
@@ -24,14 +24,14 @@ public class IssueResilience {
     }
 
     @CircuitBreaker(name = "isUserValid_cb", fallbackMethod = "isUserKnown")
-    @Bulkhead(name = "isUserValid_bh", fallbackMethod = "isUserKnown")
+    @Bulkhead(name = "isUserValid_bh", fallbackMethod = "isUserKnown", type = Bulkhead.Type.THREADPOOL)
     public boolean isUserValid(Long id) {
         ResponseEntity<Map<String, String>> response = userService.getUser(id);
         return response.getStatusCode().is2xxSuccessful();
     }
 
     @CircuitBreaker(name = "isColumnValid_cb", fallbackMethod = "isColumnKnown")
-    @Bulkhead(name = "isColumnValid_bh", fallbackMethod = "isColumnKnown")
+    @Bulkhead(name = "isColumnValid_bh", fallbackMethod = "isColumnKnown", type = Bulkhead.Type.THREADPOOL)
     public boolean isColumnValid(Long id) {
         ResponseEntity<Map<String, String>> response = columnService.getColumn(id);
         return response.getStatusCode().is2xxSuccessful();
@@ -47,7 +47,7 @@ public class IssueResilience {
             System.err.println("Usuário foi encontrado, portanto é válido");
             return true;
         } else {
-            System.err.println("Não foi encontrada issue criada pelo usuário de id="+id);
+            System.err.println("Não foi encontrada issue criada pelo usuário de id=" + id);
             return false;
         }
     }
@@ -62,7 +62,7 @@ public class IssueResilience {
             System.err.println("Column foi encontrado, portanto é válida");
             return true;
         } else {
-            System.err.println("Não foi encontrada issue associado a column de id="+id);
+            System.err.println("Não foi encontrada issue associado a column de id=" + id);
             return false;
         }
     }
--- src/main/java/ufrn/imd/jv/springissue/IssueService.java
+++ src/main/java/ufrn/imd/jv/springissue/IssueService.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
 import java.util.Optional;
 
 @Service
